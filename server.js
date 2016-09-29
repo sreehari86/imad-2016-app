95d9c7ce8bdad71064836cea199e5e990126b76a
sreehari86 <sreehari.gnair@gmail.com>
2016-09-29 21:06:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.get('/', function (req, res) {
 });
 
 app.get(':/articleName', function(req,res){
-    var articleName=res.param.articleName;
+    var articleName=req.param.articleName;
     res.send(createTemplate(articles[articleName]));
     
 });
